b24138e9cadb85424feb9d1af9c52c5c90c8033d
benkortbi <new394665@gmail.com>
2023-11-14 18:18:16 +0100

    fix bugs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const homeRouter = require("./routes/home");
 const services = require("./routes/services");
 const expressLayouts = require("express-ejs-layouts");
+const cors = require("cors");
 const path = require("path");
 const app = express();
 //set engine
@@ -12,6 +13,11 @@ app.set("views", path.join(__dirname, "views"));
 app.use(expressLayouts);
 app.use(express.static("public"));
 app.use(express.json());
+const corsOptions = {
+	origin: "https://regex-validator.onrender.com",
+	optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(corsOptions);
 //routes
 app.use("/", homeRouter);
 app.use("/validator", services);
