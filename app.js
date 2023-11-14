cff67150c1947ac7b206c1e140037ec4b3bb4460
benkortbi <new394665@gmail.com>
2023-11-14 18:25:18 +0100

    fix bugs

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const corsOptions = {
 	origin: "https://regex-validator.onrender.com",
 	optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
-app.use(corsOptions);
+app.use(cors(corsOptions));
 //routes
 app.use("/", homeRouter);
 app.use("/validator", services);
